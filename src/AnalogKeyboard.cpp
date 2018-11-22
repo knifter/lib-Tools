9410165c00782aeae8a180240b44378042eb6ca3
Tijs van Roon <tvroon1@uva.nl>
2018-11-22 15:07:46 +0100

    Fixed implementation: got double reads

--- src/AnalogKeyboard.cpp
+++ src/AnalogKeyboard.cpp
@@ -6,21 +6,28 @@
 
 Button_t AnalogKeyboard::read_buttons_once()
 {
-    Button_t key = read_buttons();
+    Button_t key1 = read_buttons();
 
-    static Button_t key_prv = BTN_NONE;
+    // Only process key if we have three identical reads
     static uint8_t cnt = 0;
-    if(key != key_prv)
+    static Button_t key = BTN_NONE;
+    if(key1 != key)
     {
+        key = key1;
         cnt = 0;
-        key_prv = key;
         return BTN_NONE;
     };
     cnt++;
-    if(cnt == CNT_MATCH)
-        return key;
+    if(cnt < CNT_MATCH)
+        return BTN_NONE;
 
-    return BTN_NONE;
+    // only return this state once
+    static Button_t key_prv = BTN_NONE;
+    if(key == key_prv)
+        return BTN_NONE;
+
+    key_prv = key;
+    return key;
 };
 
 Button_t AnalogKeyboard::read_buttons()
