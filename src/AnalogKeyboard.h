81a9089fa245e51599830f7f7b24b09ae3faafca
Tijs van Roon <tvroon1@uva.nl>
2018-11-23 15:53:14 +0100

    Add read_buttons_long, not working

--- src/AnalogKeyboard.h
+++ src/AnalogKeyboard.h
@@ -4,13 +4,19 @@
 #include <Arduino.h>
 #include <stdint.h>
 
+// Key identifiers are chosen specifically: long presses have 0x01 set, short do not!
 typedef enum {
-    BTN_RIGHT = 0,
-    BTN_UP = 99 - 50,
-    BTN_DOWN = 256 - 50,
-    BTN_LEFT = 409 - 50,
-    BTN_SELECT = 639 - 50,
-    BTN_NONE = 1000,
+    BTN_NONE = 0,
+    BTN_RIGHT = 2,
+    BTN_RIGHT_LONG,
+    BTN_UP,
+    BTN_UP_LONG,
+    BTN_DOWN,
+    BTN_DOWN_LONG,
+    BTN_LEFT,
+    BTN_LEFT_LONG,
+    BTN_SELECT,
+    BTN_SELECT_LONG,
 } Button_t;
 
 class AnalogKeyboard
@@ -18,6 +24,7 @@ class AnalogKeyboard
 public:
     AnalogKeyboard(const uint8_t analog_pin = PIN_A0) : _pin(analog_pin) {};
         
+    Button_t read_buttons_long();
     Button_t read_buttons_once();
     Button_t read_buttons();
 
