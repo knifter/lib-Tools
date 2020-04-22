cf0632dde03fc89e53f79aeec98f4b3f3b4e24ba
Tijs van Roon <t.vanroon@uva.nl>
2020-04-22 13:22:06 +0200

    long/short time

--- src/keytool.h
+++ src/keytool.h
@@ -1,10 +1,11 @@
-#include <stdint.h>
+#pragma once
 
+#include <stdint.h>
 
 #define KEYTOOL_LONG_MASK    ((uint32_t)(0x80000000))
 #define KEYTOOL_KEY_NONE     ((uint32_t)(0x00000000))
-#define KEYTOOL_LONG_MS      2000
-#define KEYTOOL_SHORT_MS     200
+#define KEYTOOL_LONG_MS      1500
+#define KEYTOOL_SHORT_MS     50
 #define KEYTOOL_HOLDOFF_MS	 0
 
 uint32_t keytool_get_event(uint32_t keystate);
