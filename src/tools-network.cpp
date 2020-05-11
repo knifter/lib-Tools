ed62faccbae072fbff89c796653f85972edc2517
Tijs van Roon <t.vanroon@uva.nl>
2020-05-11 13:48:40 +0200

    Renamed to tools-*, add tools-network

---
+++ src/tools-network.cpp
@@ -0,0 +1,22 @@
+#include "util.h"
+
+#include <Arduino.h>
+#include <WiFi.h>
+
+void printMAC()
+{
+	uint8_t mac[6];
+	esp_read_mac(mac, ESP_MAC_WIFI_STA);
+	// WiFi.macAddress(mac); // Does not return right mac address before connect
+	Serial.print(mac[5],HEX);
+	Serial.print(":");
+	Serial.print(mac[4],HEX);
+	Serial.print(":");
+	Serial.print(mac[3],HEX);
+	Serial.print(":");
+	Serial.print(mac[2],HEX);
+	Serial.print(":");
+	Serial.print(mac[1],HEX);
+	Serial.print(":");
+	Serial.print(mac[0],HEX);
+};
