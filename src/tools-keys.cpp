ef588cf4818819e49609aa03a2a2aad3932a049c
Tijs van Roon <t.vanroon@uva.nl>
2021-11-30 17:44:55 +0100

    key2event 2nd version: short, long, repeat, release events

--- src/tools-keys.cpp
+++ src/tools-keys.cpp
@@ -4,66 +4,89 @@
 
 #include <Arduino.h> // millis(), 
 
-uint32_t keytool_get_event(uint32_t keystate)
+#include "tools-log.h"
+
+// higher bits are taken for events
+#define KEYTOOL_MODIFIER_MASK	(KEYTOOL_SHORT | KEYTOOL_LONG | KEYTOOL_LONG_REPEAT | KEYTOOL_RELEASED)
+#define KEYTOOL_KEYS_MASK       (~KEYTOOL_MODIFIER_MASK)    
+
+uint32_t key2event(uint32_t pressed)
 {
-	static uint32_t last = KEYTOOL_KEY_NONE;
+	static uint32_t last = KEYTOOL_NONE;
 	static uint32_t start = 0;
 
-    keystate = keystate & (~KEYTOOL_LONG_MASK);
-
-	// Hold off our cycle if we're called too soon
 	uint32_t now = millis();
-#if KEYTOOL_HOLDOFF_MS > 0
-	static uint32_t lasttime = 0;
-	if(now - lasttime < KEYTOOL_HOLDOFF_MS)
-		return KEYTOOL_KEY_NONE;
-	lasttime = now;
-#endif
 
-	// Serial.printf("pressed: %x, last: %x, start = %u, now = %u, hold = %d\n", pressed, last, start, now, now - start);
+	// Remove the modifiers, if given any (should not)
+    pressed &= KEYTOOL_KEYS_MASK;
 
-	// Did we release a key?
-	if(keystate == KEYTOOL_KEY_NONE)
+	// No key press or release
+	if(pressed == KEYTOOL_NONE)
 	{
-		// last one was long press, now released, which wont be a short press
-		if(last & KEYTOOL_LONG_MASK) 
+        // if no keys were registered, this is not a release
+        if(!last)
+            return KEYTOOL_NONE;
+
+		// last one was long press which is now released, we already sent out an event so skip sending a short
+		if(last & KEYTOOL_LONG)
 		{
-			last = KEYTOOL_KEY_NONE;
+            // DBG("released after long");
+			last = KEYTOOL_NONE;
 			start = now;
-		}
+            return KEYTOOL_RELEASED;
+		};
 
 		// Did we release a key?
-		uint32_t ret = KEYTOOL_KEY_NONE;
+		uint32_t ret = KEYTOOL_NONE;
 		// Short press detect, only if long enough will we return it
 		if((now - start) > KEYTOOL_SHORT_MS)
-			ret = last;
+			ret = last | KEYTOOL_SHORT;
+
 		// reset detector
-		last = KEYTOOL_KEY_NONE;
+		last = KEYTOOL_NONE;
 		start = now;
 		return ret;
 	};
 
-	// so we have a key pressed from now on
+	// so we have a (or more) key pressed from now on
 
-	// first press down?
-	if(!(last & KEYTOOL_LONG_MASK) && last != keystate)
+	// If pressed keys are added, we restart counting. But ignore modifier bits for that
+    uint32_t last_keys = last & KEYTOOL_KEYS_MASK;
+    if(last_keys != (pressed | last_keys))
 	{
-		last = keystate; 
+        // keys are added, modifiers removed
+		last = (last | pressed) & KEYTOOL_KEYS_MASK; 
 		start = now;
+        return KEYTOOL_NONE;
 	};
 
 	// Long press detect
-	if(now - start > KEYTOOL_LONG_MS)
-	{
-		if(last & KEYTOOL_LONG_MASK) // long press already detected
-			return KEYTOOL_KEY_NONE;
+    if(last & KEYTOOL_LONG)
+    {
+        // LONG already set: waiting for LONG_REPEAT
+        if(now - start > KEYTOOL_LONG_REPEAT_MS)
+        {
+            // this is LONG_REPEAT, we need to remove modifiers, only add LONG_REPEAT
+            // but also send full key detected key mask to prevent AB_LONG->A_LONG migration
+            // while releasing keys
+            start = now;
+            return last_keys | KEYTOOL_LONG_REPEAT; // for all previously pressed keys
+        };
+    }else{
+        // LONG not set: waiting for LONG
+        if(now - start > KEYTOOL_LONG_MS)
+        {
+            // Mark as LONG and return this
+            last |= KEYTOOL_LONG;
+            start = now;
+            return last;
+        };
+    };
 
-		last |= KEYTOOL_LONG_MASK; // make it a long press
-		return last;
+    // nothing, just chillin' until LONG or LONG_REPEAT
+	return KEYTOOL_NONE;
+};
 
-		// // last = KEY_NONE; // to prevent short key detect
-		// start = now;
-	};
-
-	return KEYTOOL_KEY_NONE;
-}
+#if KEYTOOL_HOLDOFF_MS > 0
+    #error "HOLDOFF no longer supported. Implement in calling loop."
+#endif
--- src/tools-keys.h
+++ src/tools-keys.h
@@ -2,10 +2,13 @@
 
 #include <stdint.h>
 
-#define KEYTOOL_LONG_MASK    ((uint32_t)(0x80000000))
-#define KEYTOOL_KEY_NONE     ((uint32_t)(0x00000000))
-#define KEYTOOL_LONG_MS      1500
-#define KEYTOOL_SHORT_MS     50
-#define KEYTOOL_HOLDOFF_MS	 0
+#define KEYTOOL_NONE           ((uint32_t)(0x00000000))
+#define KEYTOOL_RELEASED       ((uint32_t)(0x10000000)) 
+#define KEYTOOL_SHORT          ((uint32_t)(0x20000000))
+#define KEYTOOL_LONG           ((uint32_t)(0x40000000))
+#define KEYTOOL_LONG_REPEAT    ((uint32_t)(0x80000000))
+#define KEYTOOL_SHORT_MS       50
+#define KEYTOOL_LONG_MS        1000
+#define KEYTOOL_LONG_REPEAT_MS 100
 
-uint32_t keytool_get_event(uint32_t keystate);
+uint32_t key2event(uint32_t keystate);
