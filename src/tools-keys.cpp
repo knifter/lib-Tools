ee4c8a99d9aa5e7570e06dea7f3c55ca4e20482f
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2023-08-11 15:29:15 +0200

    Emit LONG_RELEASED & RELEASED

--- src/tools-keys.cpp
+++ src/tools-keys.cpp
@@ -10,9 +10,84 @@
 #define KEYTOOL_MODIFIER_MASK	(KEYTOOL_SHORT | KEYTOOL_LONG | KEYTOOL_LONG_REPEAT | KEYTOOL_LONG_RELEASED)
 #define KEYTOOL_KEYS_MASK       (~KEYTOOL_MODIFIER_MASK)    
 
-/*
-    Debug code:
-	switch(e)
+/* example code
+typedef enum : uint32_t
+{
+	KEY_NONE = 0x00,
+    KEY_RELEASED = KEYTOOL_RELEASED,
+
+    // key defs
+	KEY_A = 0x01,
+	KEY_B = 0x02,
+	KEY_C = 0x04,
+
+	// combinations
+	KEY_AB = KEY_A | KEY_B,
+	KEY_AC = KEY_A | KEY_C,
+	KEY_BC = KEY_B | KEY_C,
+	KEY_ABC = KEY_A | KEY_B | KEY_C,
+
+    // single key modes
+    KEY_A_SHORT = KEY_A | KEYTOOL_SHORT,
+    KEY_B_SHORT = KEY_B | KEYTOOL_SHORT,
+    KEY_C_SHORT = KEY_C | KEYTOOL_SHORT,
+    KEY_A_LONG = KEY_A | KEYTOOL_LONG,
+    KEY_B_LONG = KEY_B | KEYTOOL_LONG,
+    KEY_C_LONG = KEY_C | KEYTOOL_LONG,
+    KEY_A_LONG_REPEAT = KEY_A | KEYTOOL_LONG_REPEAT,
+    KEY_B_LONG_REPEAT = KEY_B | KEYTOOL_LONG_REPEAT,
+    KEY_C_LONG_REPEAT = KEY_C | KEYTOOL_LONG_REPEAT,
+    KEY_A_LONG_RELEASED = KEY_A | KEYTOOL_LONG_RELEASED,
+    KEY_B_LONG_RELEASED = KEY_B | KEYTOOL_LONG_RELEASED,
+    KEY_C_LONG_RELEASED = KEY_C | KEYTOOL_LONG_RELEASED,    
+
+    // combinations of 2
+	KEY_AB_SHORT = KEY_AB | KEYTOOL_SHORT,
+	KEY_AC_SHORT = KEY_AC | KEYTOOL_SHORT,
+	KEY_BC_SHORT = KEY_BC | KEYTOOL_SHORT,
+	KEY_AB_LONG = KEY_AB | KEYTOOL_LONG,
+	KEY_AC_LONG = KEY_AC | KEYTOOL_LONG,
+	KEY_BC_LONG = KEY_BC | KEYTOOL_LONG,
+	KEY_AB_LONG_REPEAT = KEY_AB | KEYTOOL_LONG_REPEAT,
+	KEY_AC_LONG_REPEAT = KEY_AC | KEYTOOL_LONG_REPEAT,
+	KEY_BC_LONG_REPEAT = KEY_BC | KEYTOOL_LONG_REPEAT,
+
+    // combinations of 3
+	KEY_ABC_SHORT = KEY_ABC | KEYTOOL_SHORT,
+	KEY_ABC_LONG = KEY_ABC | KEYTOOL_LONG,
+	KEY_ABC_LONG_REPEAT = KEY_ABC | KEYTOOL_LONG_REPEAT,
+} demo_event_t;
+
+const char* keytool_event_name(demo_event_t e)
+{
+	if(e == KEY_NONE)
+		return "KEY_NONE";
+	if(e == KEY_RELEASED)
+		return "KEY_RELEASED";
+
+    static char buf[sizeof("KEY_ABC_SHORT_LONG_REPEAT_RELEASED")+1];
+	strcpy(buf, "KEY_");                                        // 4
+	if(e & KEY_A)               strcat(buf, "A");               // 1
+	if(e & KEY_B)               strcat(buf, "B");               // 1
+	if(e & KEY_C)               strcat(buf, "C");               // 1
+	if(e & KEYTOOL_SHORT)       strcat(buf, "_SHORT");          // 6
+	if(e & KEYTOOL_LONG)        strcat(buf, "_LONG");           // 5
+	if(e & KEYTOOL_LONG_REPEAT) strcat(buf, "_LONG_REPEAT");    // 12
+    if(e & KEYTOOL_RELEASED)    strcat(buf, "_RELEASED");       // 9
+	return buf;
+};
+
+void main()
+{
+    uint32_t keys = 0x00;
+    if(digitalRead(PIN_KEY_A))
+        keys |= KEY_A;
+    if(digitalRead(PIN_KEY_B))
+        keys |= KEY_B;
+    if(digitalRead(PIN_KEY_C))
+        keys |= KEY_C;
+    test_event_t event = static_cast<demo_event_t>(key2event(keys));
+	switch(event)
 	{
 		case KEY_A:                 Serial.println("KEY_A"); break;
 	    case KEY_B:                 Serial.println("KEY_B"); break;
@@ -37,10 +112,19 @@ uint32_t key2event(uint32_t pressed)
 
 	uint32_t now = millis();
 
+    // If a released is queued, emit that first
+    // After a press dissapears, a KEYTOOL_RELEASED is emitted
+    if(last == KEYTOOL_RELEASED)
+    {
+        last = KEYTOOL_NONE;
+        return KEYTOOL_RELEASED;
+    };
+
 	// Remove the modifiers, if given any (should not)
     pressed &= KEYTOOL_KEYS_MASK;
 
 	// No key press or release
+    uint32_t last_keys = last & KEYTOOL_KEYS_MASK;
 	if(pressed == KEYTOOL_NONE)
 	{
         // if no keys were registered, this is not a release
@@ -51,9 +135,9 @@ uint32_t key2event(uint32_t pressed)
 		if(last & KEYTOOL_LONG)
 		{
             // DBG("released after long");
-			last = KEYTOOL_NONE;
+			last = KEYTOOL_RELEASED;
 			start = now;
-            return KEYTOOL_LONG_RELEASED;
+            return last_keys | KEYTOOL_LONG_RELEASED;
 		};
 
 		// Did we release a key?
@@ -63,7 +147,7 @@ uint32_t key2event(uint32_t pressed)
 			ret = last | KEYTOOL_SHORT;
 
 		// reset detector
-		last = KEYTOOL_NONE;
+		last = KEYTOOL_RELEASED;
 		start = now;
 		return ret;
 	};
@@ -71,7 +155,6 @@ uint32_t key2event(uint32_t pressed)
 	// so we have a (or more) key pressed from now on
 
 	// If pressed keys are added, we restart counting. But ignore modifier bits for that
-    uint32_t last_keys = last & KEYTOOL_KEYS_MASK;
     if(last_keys != (pressed | last_keys))
 	{
         // keys are added, modifiers removed
--- src/tools-keys.h
+++ src/tools-keys.h
@@ -6,7 +6,9 @@
 #define KEYTOOL_SHORT          ((uint32_t)(0x10000000))
 #define KEYTOOL_LONG           ((uint32_t)(0x20000000))
 #define KEYTOOL_LONG_REPEAT    ((uint32_t)(0x40000000))
-#define KEYTOOL_LONG_RELEASED  ((uint32_t)(0x80000000)) 
+#define KEYTOOL_RELEASED       ((uint32_t)(0x80000000))
+#define KEYTOOL_LONG_RELEASED  (KEYTOOL_LONG | KEYTOOL_RELEASED) 
+#define KEYTOOL_EVENT_MASK     ((uint32_t)(0xF0000000))
 #define KEYTOOL_SHORT_MS       50
 #define KEYTOOL_LONG_MS        1000
 #define KEYTOOL_LONG_REPEAT_MS 100
