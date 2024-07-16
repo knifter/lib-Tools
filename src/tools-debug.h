52f9cdeeb401f6965860409fa805563b563d2995
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2024-07-16 11:11:17 +0200

    DBG_PTR only on DEBUG build

--- src/tools-debug.h
+++ src/tools-debug.h
@@ -1,10 +1,18 @@
 #ifndef __TOOLS_PTRDBG_H
 #define __TOOLS_PTRDBG_H
 
+#ifdef DEBUG
+
 bool 		dbg_ptr_register(void* ptr, const char* name);
 const char* dbg_ptr_lookup(void* ptr);
 
-#define DBG_PTR_REG(ptr, name)		dbg_ptr_register(ptr, name)
-#define DBG_PTR_LOOKUP(ptr, name)	dbg_ptr_lookup(ptr, name)
+
+	#define DBG_PTR_REGISTER(ptr, name)		dbg_ptr_register(ptr, name)
+	#define DBG_PTR_NAME(ptr)				dbg_ptr_lookup(ptr)
+
+#else
+	#define DBG_PTR_REGISTER(ptr, name)		
+	#define DBG)PTR_NAME(ptr)
+#endif
 
 #endif // __TOOLS_PTRDBG_H
